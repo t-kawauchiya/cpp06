2e28012f0cbc2e3407a7ff0caf023d773455fbb7
takawauc <takawauc@student.42tokyo.jp>
2026-02-25 16:46:31 +0900

    refactor:

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: takawauc <takawauc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 16:56:41 by takawauc          #+#    #+#             */
-/*   Updated: 2026/02/18 19:59:22 by takawauc         ###   ########.fr       */
+/*   Updated: 2026/02/25 16:40:58 by takawauc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,12 @@
 
 int main() {
   srand((unsigned int)std::time(NULL));
-  std::cout << "[void identify(Base* p)]" << std::endl;
+
   Base* test = generate();
+
+  std::cout << "[void identify(Base* p)]" << std::endl;
   identify(test);
+
   std::cout << "[void identify(Base& p)]" << std::endl;
   identify(*test);
 }
